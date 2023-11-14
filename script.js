0cf127033cd11c2c7cf9e4a4b0fd17d597169221
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-14 09:07:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var dateDiffInDays = function (date1, date2) {
 	const date1Ms = new Date(date1);
 	const date2Ms = new Date(date2);
 
-	const diffMs = date1Ms.getTime() - date2Ms.getTime();
+	const diffMs = date2Ms.getTime() - date1Ms.getTime();
 	const diffDays = diffMs / (1000*60*60*24);
 	 
 	return diffDays;
