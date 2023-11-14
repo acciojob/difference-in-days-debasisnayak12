4b6398da5799016448f28f141af10610f7bbd512
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-14 09:05:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const date1Ms = new Date(date1);
+	const date2Ms = new Date(date2);
+
+	const diffMs = date1Ms.getTime() - date2Ms.getTime();
+	const diffDays = diffMs / (1000*60*60*24);
+	 
+	return diffDays;
+  
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
+
 alert(dateDiffInDays(dateOne, dateTwo));
